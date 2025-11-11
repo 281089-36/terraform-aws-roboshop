6ffbceb0883d7db66d1b8a1aca30c8ce665240e3
msivakumarreddy <info@joindevops.com>
2025-11-11 17:07:16 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+} 
+
+variable "environment" {
+    default = "dev"
+} 
+
+variable "zone_id" {
+    default = "Z03585001XMLH4GCJ3SIY"
+} 
+
+variable "zone_name" {
+    default = "thulasi.fun"
+} 
+
+variable "component" {
+
+} 
+
+variable "rule_priority" {
+
+}
